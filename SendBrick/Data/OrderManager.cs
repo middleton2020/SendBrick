213203d3dc9d1388bc509ab95b658f71e0b5070a
Colin Middleton <microsoft@medievalresearch.co.uk>
2023-11-23 20:01:13 +0000

    Add OrderManager and tests.

---
+++ SendBrick/Data/OrderManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CM.SendBrick.Library.Data
+{
+    public class OrderManager
+    {
+        private Dictionary<int, IOrderData> _ordersList = new Dictionary<int, IOrderData>();
+        private int _sequence = -1;
+
+        public int AddOrder(IOrderData OrderData)
+        {
+            _sequence += 1;
+            _ordersList.Add(_sequence, OrderData);
+
+            return _sequence;
+        }
+
+        public IOrderData GetOrder(int Instance)
+        {
+            IOrderData orderRecord = null;
+
+            if (Instance < 0 || Instance > _sequence)
+            {
+                throw new IndexOutOfRangeException("There is no order with ID " + Instance.ToString());
+            }
+
+            orderRecord = _ordersList[Instance];
+            if (orderRecord == null)
+            {
+                throw new KeyNotFoundException("No valid order with ID " + Instance.ToString());
+            }
+
+            return orderRecord;
+        }
+    }
+}
---
+++ SendBrickTests/Data/OrderManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CM.SendBrick.Library.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.SendBrick.Library.Tests
+{
+    [TestClass()]
+    public class OrderManagerTests
+    {
+        [TestMethod()]
+        public void AddOneOrderTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void AddMultipleOrdersTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
